763d63bf14738abcf8a888e7ac0b2b68ccc01db6
Eliezer Poliakov <poliakoveliezer@gmail.com>
2026-08-05 14:54:20 +0300

    bootstrap Spring Boot backend and React frontend

---
+++ backend/src/test/java/com/poliakov/taxplatform/TaxPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poliakov.taxplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
